b0a925f8aeae22531f019ce4e7803d735dc72371
SebastianJaroszek <sebastian_jaroszek@emstories.pl>
2018-02-13 20:56:55 +0100

    changes in WelcomeServlet

--- src/main/java/pl/sebastian/WelcomeServlet.java
+++ src/main/java/pl/sebastian/WelcomeServlet.java
@@ -1,6 +1,8 @@
 package pl.sebastian;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,8 +10,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/welcome")
+@WebServlet(urlPatterns = {"/welcome", "/hello", "/hi"},
+        initParams = {
+                @WebInitParam(name = "company", value = "Software Development Academy"),
+                @WebInitParam(name = "year", value = "2018")
+        })
 public class WelcomeServlet extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        //TODO odczytać parametry
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
