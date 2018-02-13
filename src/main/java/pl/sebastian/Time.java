5cdf6d9b3ecf44e5817d9b93eb7e44fab9358d4d
SebastianJaroszek <sebastian_jaroszek@emstories.pl>
2018-02-13 20:29:24 +0100

    add Time class, changes in web.xml

--- src/main/java/pl/sebastian/Time.java
+++ src/main/java/pl/sebastian/Time.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -15,8 +16,14 @@ public class Time extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
         PrintWriter out = resp.getWriter();
-        out.println("<h1>Dzisiaj jest: " + LocalDate.now() + "</h1>");
+        LocalDate localDate = LocalDate.now();
+        out.println("<h1>Dzisiaj jest: " + localDate + "</h1>");
         out.println("<h1>Godzina: " + LocalTime.now() + "</h1>");
+
+        if (localDate.getDayOfWeek().equals(DayOfWeek.TUESDAY)) {
+            out.println("piątek!");
+        }
     }
 }
